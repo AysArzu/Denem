4035352651c43dc8829241dae01c27fcd0553375
AysArzu <aysebattal@gmail.com>
2023-03-01 10:15:01 +0300

    versiyon

--- src/main/java/GithubGrup.java
+++ src/main/java/GithubGrup.java
@@ -1,5 +1,10 @@
 public class GithubGrup {
     public static void main(String[] args) {
-
+        System.out.println("gunaydin");
+        System.out.println("gunaydin");
+        System.out.println("gunaydin");
+        System.out.println("gunaydin");
+        System.out.println("gunaydin");
+        System.out.println("gunaydin");
     }
 }
